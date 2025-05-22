f238b03dbddcd89113fa178ce58a0849f038d0c0
Florian Franziskakis <florian.franziskakis@unige.ch>
2025-05-22 21:21:19 +0200

    Added radar chart demo for land areas

--- geo_indicators/visualization.py
+++ geo_indicators/visualization.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from geo_indicators.utils import get_output_csv_path
 
 def plot_mask(mask, title):
     plt.figure(figsize=(10, 6))
@@ -55,4 +58,54 @@ def plot_timeseries_double(ages, metric1, metric1_name, metric2, metric2_name, t
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def heatmap_chart(source, version):
+    df_path = get_output_csv_path(source, version)
+    df = pd.read_csv(df_path)
+
+    plt.figure(figsize=(12, 8))
+    heatmap = sns.heatmap(df.drop('Age', axis=1).corr(), annot=True, cmap='coolwarm', linewidths=0.5)
+
+    # Adjust label rotation and font size
+    heatmap.set_xticklabels(heatmap.get_xticklabels(), rotation=45, horizontalalignment='right', fontsize=10)
+    heatmap.set_yticklabels(heatmap.get_yticklabels(), rotation=0, fontsize=10)
+
+    plt.tight_layout()  # Adjust layout to ensure everything fits within the plot area
+    plt.show()
+
+def radar_chart(source,version):
+    df_path = get_output_csv_path(source, version)
+    df = pd.read_csv(df_path)
+    area_columns = [
+    'Land_Area', 'Polar_Land_Area', 'Temperate_Land_Area', 'Subtropical_Land_Area',
+    'Tropical_Land_Area', 'Northern_Land_Area', 'Southern_Land_Area', 'High_Altitude_Area',
+    'Continental_Shelves_Area'
+    ]
+    num_vars = len(area_columns)
+    angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
+    angles += angles[:1]
+    fig, ax = plt.subplots(figsize=(10, 10), subplot_kw=dict(polar=True))
+    for i, row in df.iterrows():
+        values = row[area_columns].tolist()
+        values += values[:1]
+        ax.plot(angles, values, linewidth=1, linestyle='solid', label=f'Age {row["Age"]}')
+        ax.fill(angles, values, alpha=0.25)
+
+    ax.set_yticklabels([])
+    ax.set_xticks(angles[:-1])
+    ax.set_xticklabels(area_columns)
+    plt.legend(loc='upper right', bbox_to_anchor=(1.1, 1.1))
+    plt.show()
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # main.py
 
 from geo_indicators.stats import generate_stats
+from geo_indicators.visualization import heatmap_chart, radar_chart
 
 if __name__ == "__main__":
     source = "PANALESIS"
     version = "v1"
-    generate_stats(source,version)
+    #generate_stats(source,version)
+    heatmap_chart(source,version)
+    radar_chart(source,version)
+
 
